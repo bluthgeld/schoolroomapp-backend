b25a84b2cee4c9652a48d6e8118f42178946d949
Robert Pedersen <rob@worldcrimeleague.org>
2019-08-13 13:11:29 -0400

    add create to announcement and message controllers

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -32,6 +32,23 @@ class AnnouncementsController < ApplicationController
         }
       }
     )
+  end
+
+  def create
+
+    new_announcement = Announcement.create(announcement_params)
+    render json: new_announcement.to_json(
+      :include => {
+        :initiator => {
+          :only => [:id, :username, :first_name, :last_name, :picture]
+        }
+      }
+    )
 
   end
+
+  private
+    def announcement_params
+      params.require(:announcement).permit(:initiator_id, :receiver_id, :subject, :body)
+    end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,37 @@ class MessagesController < ApplicationController
 
   end
 
+  def show
 
+    message = Message.find_by(id: params[:id])
+
+    render json: message.to_json(
+
+      :include => {
+        :sender => {
+          :only => [:id, :username, :first_name, :last_name, :picture]
+        }
+      }
+    )
+
+  end
+
+  def create
+
+    new_message = Message.create(message_params)
+    render json: new_message.to_json(
+      :include => {
+        :sender => {
+          :only => [:id, :username, :first_name, :last_name, :picture]
+        }
+      }
+    )
+
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:announcement_id, :sender_id, :body)
+    end
 
 end
